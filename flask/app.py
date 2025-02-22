655bc32c6658324f6174a6b11d6ecc23e23204ad
Sofia <sophia.konovalova2@gmail.com>
2025-02-22 23:46:58 +0100

    simple flask fetch

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask with Docker!"
+def get_db_connection():
+    conn = psycopg2.connect(host='db',
+                            database='root',
+                            user='root',
+                            password='password')
+    return conn
+
+@app.route('/names')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM name_basics LIMIT 5;')
+    names = cur.fetchall()
+    cur.close()
+    conn.close()
+    return names
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
