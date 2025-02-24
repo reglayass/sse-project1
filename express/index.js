3937e01f6a3fa47290ee78669c6f5bf1a5fa02ae
Sofia <sophia.konovalova2@gmail.com>
2025-02-24 19:13:34 +0100

    simple express app setup

--- express/index.js
+++ express/index.js
@@ -1,8 +1,28 @@
 import express from 'express'
-const app = express();
+import pg from 'pg'
 
-app.get('/', (req, res) => {
-    res.send('Hello from Dockerized Express.js!');
+const { Client } = pg;
+
+const client = new Client({
+    host: 'db',
+    port: 5432,
+    user: 'root',
+    password: 'password',
+    database: 'root'
+})
+
+client.connect();
+
+const app = express();
+app.use(express.json())
+
+app.get('/names', async (req, res) => {
+    try {
+        const result = await client.query("SELECT * FROM name_basics LIMIT 5")
+        res.json(result.rows)
+    } catch (err) {
+        res.status(500).send(err)
+    }
 });
 
 const PORT = process.env.PORT || 3000;
